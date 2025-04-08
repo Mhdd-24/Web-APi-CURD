5b9766eb3f2381b18bfdc1d1da5657ebdedf94ff
Mohammed Rafi <rafirahmath2@gmail.com>
2025-04-09 01:59:09 +0530

    Remove project files and NuGet cache for EmployeeAdminPortal

--- EmployeeAdminPortal/Models/AppUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace EmployeeAdminPortal.Models
-{
-    public class AppUser: IdentityUser
-    {
-        public string? FullName { get; set; }
-    }
-}
