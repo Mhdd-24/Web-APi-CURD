b59aa3881c4fbdec74628a088caa9fde4b9d89fb
Mohammed Rafi <rafirahmath2@gmail.com>
2025-04-09 01:32:22 +0530

    Add .gitignore and update assembly informational version

--- EmployeeAdminPortal/obj/Debug/net9.0/EmployeeAdminPortal.AssemblyInfo.cs
+++ EmployeeAdminPortal/obj/Debug/net9.0/EmployeeAdminPortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e7a3c91afe7489488bfd295f88a53dacace0fc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeAdminPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
